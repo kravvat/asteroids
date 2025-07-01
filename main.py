ef742825c57b5088a28cb1d60e3510c5626f5663
kravvat <kacperstec3d@gmail.com>
2025-07-01 13:46:42 +0200

    Add logic for locking FPS

--- main.py
+++ main.py
@@ -4,14 +4,22 @@ from constants import *
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # Sets game window size
 
-    while True:
+    clock = pygame.time.Clock() # 'Clock' class will provide methods to help control a game's framerate
+    dt = 0 # This will be a variable to store delta time
+
+    while True: # Infinite game loop
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT: # Enables exiting the game by clicking on 'X'
                 return
-        screen.fill("black")
-        pygame.display.flip()
+            
+        screen.fill("black") # Fills background with black
+        pygame.display.flip() # Refreshes screen
+
+        dt = clock.tick(60) / 1000 # 'Tick' method locks FPS to 60 and returns the number of ms since the last frame
+                                   # Dividing it by 1000 converts ms to seconds
+                                   # This gives us new 'dt' (delta time)
 
 
 if __name__ == "__main__":
