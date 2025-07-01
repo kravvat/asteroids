10256c7f3c9771d48c133314fa324cd4c053f36f
kravvat <kacperstec3d@gmail.com>
2025-07-01 14:52:11 +0200

    Refactor all methods

--- circleshape.py
+++ circleshape.py
@@ -1,16 +1,12 @@
 import pygame
-from main import *
-from player import *
 
-# Base class for game objects
+
 class CircleShape(pygame.sprite.Sprite):
     def __init__(self, x, y, radius):
-        # we will be using this later
         if hasattr(self, "containers"):
             super().__init__(self.containers)
         else:
             super().__init__()
-
         self.position = pygame.Vector2(x, y)
         self.velocity = pygame.Vector2(0, 0)
         self.radius = radius
